cb56e690fda94441a6382ba4a8a72c17c140e9e0
tiana523 <127056849+tiana523@users.noreply.github.com>
2023-09-13 15:18:28 +1000

    Q1.0_Finished

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,16 @@
 import matplotlib.pyplot as plt
+import sqlite3
+
+connection = sqlite3.connect('climate.db')
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM ClimateData")
+data = cursor.fetchall()
+connection.close()
         
-years = []
-co2 = []
-temp = []
+years = [row[0] for row in data]
+co2 = [row[1] for row in data]
+temp = [row[2] for row in data]
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
